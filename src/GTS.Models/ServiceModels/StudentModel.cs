ff6cef1f309eada7d865239814ae950cc14833ca
Jimud1 <jimud1.james@gmail.com>
2017-08-24 16:16:10 +0100

    Adding test (Student) repository, service and controller
    Fixed errors when projects referencing each other

---
+++ src/GTS.Models/ServiceModels/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GTS.Models.ServiceModels
+{
+    public class StudentModel
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string EnrollmentNo { get; set; }
+    }
+}
